dcfe37b70898109890f87644d9a02f0ef50b76d0
SirBonn <byronvasquez21@gmail.com>
2024-03-06 10:00:52 -0600

    agregamos manager para archivos csv

--- src/main/java/com/srbn/utils/CSVmagnament/CSVManager.java
+++ src/main/java/com/srbn/utils/CSVmagnament/CSVManager.java
@@ -1,7 +1,26 @@
-package com.srbn.Utils;
+package com.srbn.utils.CSVmagnament;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CSVManager {
 
+        public static String readCSVFile(String filePath) throws IOException, CsvValidationException {
+        StringBuilder content = new StringBuilder();
 
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                for (String value : nextLine) {
+                    content.append(value).append("\t");
+                }
+                content.append("\n");
+            }
+        }
+
+        return content.toString();
+    }
 
 }
